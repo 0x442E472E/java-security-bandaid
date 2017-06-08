9dac5b99ab0d9f1f6177596af25c36ee1c2c04b3
Dominik <dgrodt@outlook.de>
2017-06-08 13:21:51 +0200

    fixed OWASP xml parsing for certain situations

--- owasp-dependency-check-processor/src/main/java/oss/security/bandaid/owasp/dependencycheck/owasp/reader/xmlformat/Vulnerabilities.java
+++ owasp-dependency-check-processor/src/main/java/oss/security/bandaid/owasp/dependencycheck/owasp/reader/xmlformat/Vulnerabilities.java
@@ -1,5 +1,6 @@
 package oss.security.bandaid.owasp.dependencycheck.owasp.reader.xmlformat;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 /**
  * Created by 0x442E472E on 26.05.2017.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Vulnerabilities {
     @JacksonXmlElementWrapper(useWrapping=false)
     public List<Vulnerability> vulnerability;
